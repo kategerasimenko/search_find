d82ac07d771f7c417565498ee23a2928b8b49a00
kategerasimenko <katgerasimenko@gmail.com>
2018-02-13 04:00:53 +0400

    added meaning layer

--- search_find_verbs/views.py
+++ search_find_verbs/views.py
@@ -23,10 +23,18 @@ def SemanticFieldView(request,pk):
         # to do: sort as langs sorted, restore missing values (e.g. no example for japanese)
         langex = LanguageExample.objects.filter(meaning=mex)
         meaning_pk[mex.meaning.pk].append((mex,langex))
-    meaning_list = [(Meaning.objects.get(pk=k),len(v),v) for k,v in meaning_pk.items()] # уровень значений
+    meaning_list = [(Meaning.objects.get(pk=k),len(v),v) for k,v in meaning_pk.items()] # уровень значений + их свойств
     meaning_list = sorted(meaning_list,key=lambda x: (x[0].meaning_type.meaning_type,x[0].meaning))
     meaning_list = [(k, list(g)) for k,g in groupby(meaning_list, key=lambda x: (x[0].meaning_type.meaning_type))] # уровень типов значений
-    meaning_list = [(x[0],sum([y[1] for y in x[1]]),x[1]) for x in meaning_list]
+    for i,item in enumerate(meaning_list):
+        n = 0
+        subgroups = []
+        for k,g in groupby(item[1], key=lambda x: (x[0].meaning)): # уровень только значений
+            group = list(g)
+            subn = sum([x[1] for x in group])
+            subgroups.append((k,subn,group))
+            n += subn
+        meaning_list[i] = (item[0],n,subgroups)
     return render(request,template_name,{'field':semfield.field,'langs':langs,'all_meanings':meaning_list})
     #return HttpResponse(html.escape(pprint.pformat(meaning_list)))
     
